b372e1621a0656fd2fb8a2b0e407c3605bfbec2a
Rakibul Islam <39417905+itsrakibul@users.noreply.github.com>
2019-11-28 00:46:13 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb+srv://itsrakibul:<Rakibul.18>@cluster0-otrce.mongodb.net/test?retryWrites=true&w=majority",
+    url: "mongodb+srv://itsrakibul:Tuhin.18@cluster0-otrce.mongodb.net/test?retryWrites=true&w=majority",
     serverport: process.env.PORT || 3000
 }
